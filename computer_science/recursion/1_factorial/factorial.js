86e353eef826f0d6b5fa6a2bb6577216c2328500
Julio Cesar <172700125+julioldv@users.noreply.github.com>
2026-07-02 17:54:10 -0600

    Complete recursive factorial CS exercise

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,16 @@
-const factorial = function() {
+const factorial = function (n) {
+  if (
+    typeof n !== "number" ||
+    Number.isNaN(n) ||
+    !Number.isInteger(n) ||
+    n < 0
+  ) {
+    return;
+  }
 
+  if (n === 0) return 1;
+
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
