46f1fecb80a04ad460ae9d5d9b1b6e292445c8b4
Julio Cesar <172700125+julioldv@users.noreply.github.com>
2026-07-02 21:58:31 -0600

    Complete contains recursive CS exercise

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,26 @@
-const contains = function() {
-  
+const contains = function (object, value) {
+  // For each value inside the object:
+  for (const currentValue of Object.values(object)) {
+    // If the value is equal to the target:
+    if (Object.is(currentValue, value)) {
+      // return true
+      return true;
+    }
+
+    // If the value is another object:
+    if (currentValue !== null && typeof currentValue === "object") {
+      // Search inside that object.
+      // If that search returns true:
+      if (contains(currentValue, value)) {
+        // return true
+        return true;
+      }
+    }
+  }
+
+  // After checking everything:
+  // return false
+  return false;
 };
-  
 // Do not edit below this line
 module.exports = contains;
