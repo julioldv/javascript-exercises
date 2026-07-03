92384db8560b9bbc7fb418c23d11e74647b84003
Julio Cesar <172700125+julioldv@users.noreply.github.com>
2026-07-02 23:02:20 -0600

    Complete totalIntegers recursive CS exercise

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,17 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(input) {
+
+    let integerCounter = 0;
+
+    if(typeof input !== "object" || input === null) return undefined;
+
+    for (const element of Object.values(input)) {
+        if (typeof element === "number" && Number.isInteger(element)){
+            integerCounter++;
+        }else if(element !== null && typeof element === "object"){
+            integerCounter += totalIntegers(element);
+        }
+    }
+    return integerCounter;
 };
   
 // Do not edit below this line
