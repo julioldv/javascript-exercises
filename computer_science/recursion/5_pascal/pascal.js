e160f1bf3f32f78bce2c5727795a540ba12f3c84
Julio Cesar <172700125+julioldv@users.noreply.github.com>
2026-07-06 16:18:26 -0600

    Complete pascal recursive CS exercise

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,21 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  if (n === 1) {
+    return [1];
+  }
+
+  const previousRow = pascal(n - 1);
+  const currentRow = [];
+
+  for (let i = 0; i < previousRow.length + 1; i++) {
+    const left = previousRow[i - 1] || 0;
+    const right = previousRow[i] || 0;
+
+    currentRow.push(left + right);
+  }
+
+  return currentRow;
 };
+
   
 // Do not edit below this line
 module.exports = pascal;
